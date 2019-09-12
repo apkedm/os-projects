6fc7737c46170c67c0a0e2891ab640489beae7f6
Rudy Hill <rudyghill@gmail.com>
2019-09-11 19:59:21 -0600

    bugs

--- PA1/cs3753_add.c
+++ PA1/cs3753_add.c
@@ -1,13 +1,14 @@
 #include <linux/kernel.h>
 #include <linux/linkage.h>
+#include <linux/uaccess.h>
 
 asmlinkage long sys_cs3753_add(int a, int b, int * sum)
 {
 	int c;
-	printk("X = %d", x);
-	printk("Y = %d", y);
+	printk(KERN_ALERT "A = %d\n", a);
+	printk(KERN_ALERT "B = %d\n", b);
 	c = a + b;
-	*sum = c;
-	printk("Sum = %d", *sum);
+	copy_to_user(sum, &c, sizeof(int));
+	printk(KERN_ALERT "Sum = %d\n", *sum);
 	return 0;
 }
--- PA1/test.c
+++ PA1/test.c
@@ -1,7 +1,14 @@
+#define _GNU_SOURCE
 #include <unistd.h>
 #include <sys/syscall.h>
+#include <stdio.h>
 
 int main(){
-	int err = syscall(333);
-	return err;
+	int err1 = syscall(333);
+	int a = 4;
+	int b = 5;
+	int result;
+	int err2 = syscall(334, a, b, &result);
+	printf("The result is %d\n", result);
+	return err1 & err2;
 }
